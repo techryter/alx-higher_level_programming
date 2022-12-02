b36e0871115c7b73f2b552ef4e21eb06dedd2c01
techryter <110998231+techryter@users.noreply.github.com>
2022-12-02 19:39:04 +0100

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 4-print_hexa.py
-# Sami Birhanu <samuelbirhanu01@gmail.com>
+# Kelvin Amamize <kelvinamamize@gmail.com>
 
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
